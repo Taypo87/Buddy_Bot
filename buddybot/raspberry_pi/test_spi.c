e43415fe2046a7aa99b0f093e3c6931c6a618bff
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-18 07:00:48 -0500

    put body in an infinite loop

--- buddybot/raspberry_pi/test_spi.c
+++ buddybot/raspberry_pi/test_spi.c
@@ -6,25 +6,28 @@ int main()
 {
     char command[10];
 
-    printf("Awaiting Command!\n");
-    fgets(command , 10, stdin);
-    if (strcmp(command, "forward") == 0)
-        move_forward(3);
-    else if (strcmp(command, "backward") == 0)
-        move_back(3);
-    else if (strcmp(command, "left") == 0)
-        move_left(3);
-    else if (strcmp(command, "right") == 0)
-        move_right(3);
-    else if (strcmp(command, "dright") == 0)
-        drift_right(3);
-    else if (strcmp(command, "dleft") == 0)
-        drift_left(3);
-    else if (strcmp(command, "stop") == 0)
-        move_stop();
-    else
+    while (1)
     {
-        printf("Command not recognized! The commands are as follows.\n");
-        printf("forward\nbackward\nleft\nright\ndright\ndleft\nstop\n");
+        printf("Awaiting Command!\n");
+        fgets(command , 10, stdin);
+        if (strcmp(command, "forward") == 0)
+            move_forward(3);
+        else if (strcmp(command, "backward") == 0)
+            move_back(3);
+        else if (strcmp(command, "left") == 0)
+            move_left(3);
+        else if (strcmp(command, "right") == 0)
+            move_right(3);
+        else if (strcmp(command, "dright") == 0)
+            drift_right(3);
+        else if (strcmp(command, "dleft") == 0)
+            drift_left(3);
+        else if (strcmp(command, "stop") == 0)
+            move_stop();
+        else
+        {
+            printf("Command not recognized! The commands are as follows.\n");
+            printf("forward\nbackward\nleft\nright\ndright\ndleft\nstop\n");
+        }
     }
 }
