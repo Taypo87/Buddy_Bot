8653755487c6611811d6fc914df28c9d13d2258b
Taypo87 <taylor.poczynek87@gmail.com>
2024-04-07 10:28:19 -0500

    hello world python test node

--- buddybot/ros2_workspace/src/move_pkg/move_pkg/my_first_code.py
+++ buddybot/ros2_workspace/src/move_pkg/move_pkg/my_first_code.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import rclpy
+from rclpy.node import Node
 
-def main(arg=None):
-	rclpy.init(args=args)
+def main(args=None):
+	rclpy.init(args=args)  #must have this line to start the node
+	node = Node("py_test")
+	node.get_logger().info("Hello ROS2")
+	rclpy.shutodwn()
 
 if __name__ == "__main__":
 	main()
